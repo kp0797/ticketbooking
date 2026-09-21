d43abcd41d238e75fead13338e3a7806381967ac
Kamakshya Panda <kamakshyaprasad071997@gmail.com>
2026-09-21 17:51:57 +0530

    Test Query

--- src/main/java/com/shows/ticketbooking/show/repository/ShowSeatRepository.java
+++ src/main/java/com/shows/ticketbooking/show/repository/ShowSeatRepository.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import com.shows.ticketbooking.show.entity.SeatStatus;
 import com.shows.ticketbooking.show.entity.ShowSeat;
 
 
@@ -14,6 +16,6 @@ public interface ShowSeatRepository extends JpaRepository<ShowSeat, Long> {
     
     List<ShowSeat> findByShow_ShowIdAndStatus(
         Long showId,
-        ShowSeat status
+        SeatStatus status
     );
 }
